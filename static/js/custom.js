46e3baeddf4ebde73548eb0d09704b1386f88359
Snow <i@xkun.dev>
2023-01-20 16:43:46 +0800

    style: add background :^)

---
+++ static/js/custom.js
@@ -0,0 +1,7 @@
+$(function () {
+    $.backstretch([
+        "/background/background1.jpg",
+        "/background/background2.jpg",
+        "/background/background3.jpg",
+    ], { duration: 60000, fade: 2000 });
+});
